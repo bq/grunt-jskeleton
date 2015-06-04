b0aeaa84ad3c3fe9ca86f71bef34c722609a3612
Álvaro Brito <alvaro.brito@bq.com>
2015-06-04 22:00:24 +0200

    feat(svg) xmlns:xlink support
    Change-Id: I35b024652de99f5dab489501488fada4d08080f3

--- config/default.js
+++ config/default.js
@@ -58,7 +58,8 @@ module.exports.tasks = {
             prefix: 'icon-',
             svg: { // will add and overide the the default xmlns="http://www.w3.org/2000/svg" attribute to the resulting SVG
                 viewBox: '0 0 100 100',
-                xmlns: 'http://www.w3.org/2000/svg'
+                xmlns: 'http://www.w3.org/2000/svg',
+                'xmlns:xlink': 'http://www.w3.org/1999/xlink'
             }
         }
     }
